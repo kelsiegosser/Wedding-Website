76c3e729c057bb6e761ebb965e331d8723696308
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-09-08 12:02:35 -0400

    hook up the nav bar

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,34 +1,25 @@
-
 /************/
-/*** Map ****/
+/*** Nav ****/
 /************/
-function initMap() {
-  var myLatlng = {lat: 18.3385, lng: -64.8566};
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: myLatlng
-  });
+const offset = 15;
 
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    title: 'Click to zoom'
-  });
+$('nav ul li a').on('click', function(event) {
+  $(this).parent().find('a').removeClass('active-nav');
+  $(this).parent().addClass('active-nav');
+});
 
-  map.addListener('center_changed', function() {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-    window.setTimeout(function() {
-      map.panTo(marker.getPosition());
-    }, 3000);
+$(window).on('scroll', function() {
+  $('.section').each(function() {
+      if($(window).scrollTop() + offset >= $(this).offset().top) {
+          var id = $(this).attr('id');
+          $('nav ul li a').parent().removeClass('active-nav');
+          $('nav ul li a[href=\\#'+ id +']').parent().addClass('active-nav');
+      }
   });
+});
 
-  marker.addListener('click', function() {
-    map.setZoom(12);
-    map.setCenter(marker.getPosition());
-  });
-}
+
 
 /************/
 /* Carousel */
