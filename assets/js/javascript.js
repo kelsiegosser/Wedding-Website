d7aab2a6fe9969d91bfb3733a439ca4f484987f2
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-09-29 12:32:47 -0400

    make the whole link area clickable in nav
    so that you don't have to be precisely on the word itself

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,19 +4,19 @@
 
 const offset = 15;
 
-$('nav ul li a').on('click', function(event) {
+$('nav ul a').on('click', function(event) {
   $(this)
-    .find('a').parent()
+    // .find('a')
     .removeClass('active-nav');
-  $(this).parent().addClass('active-nav');
+  $(this).addClass('active-nav');
 });
 
 $(window).on('scroll', function() {
   $('.section').each(function() {
     if ($(window).scrollTop() + offset >= $(this).offset().top) {
       var id = $(this).attr('id');
-      $('nav ul li a').parent().removeClass('active-nav');
-      $('nav ul li a[href=\\#' + id + ']').parent().addClass('active-nav');
+      $('nav ul a').removeClass('active-nav');
+      $('nav ul a[href=\\#' + id + ']').addClass('active-nav');
     }
   });
 });
